2ae3cefe7f5306a1f437e1de25a2559ffb846a31
denis.krasovskyi <denis.krasovskyi@gmail.com>
2018-12-17 15:17:44 +0200

    Fix main bg

--- src/app/WhitePaper/WhitePaper.js
+++ src/app/WhitePaper/WhitePaper.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export default class WhitePaper extends Component {
     constructor(props) {
@@ -10,63 +10,75 @@ export default class WhitePaper extends Component {
         return (
             <div className={styles.container}>
                 {/*<section>*/}
-                    {/*<div className={styles.colLeft}>*/}
-                        {/*<img className={styles.shadow} src="/images/screenshot_2.jpg" />*/}
-                    {/*</div>*/}
-                    {/*<div className={styles.colRight}>*/}
-                        {/*<h2><b>ADAPT</b> white paper</h2>*/}
-                        {/*<button*/}
-                            {/*className={styles.button} onClick={() => window.open('https://drive.google.com/file/d/1A64LT5V8s2OE6-CKgCVp-NSxvBoVO65j/view?usp=sharing', '_target')}*/}
-                        {/*>Download</button>*/}
-                    {/*</div>*/}
+                {/*<div className={styles.colLeft}>*/}
+                {/*<img className={styles.shadow} src="/images/screenshot_2.jpg" />*/}
+                {/*</div>*/}
+                {/*<div className={styles.colRight}>*/}
+                {/*<h2><b>ADAPT</b> white paper</h2>*/}
+                {/*<button*/}
+                {/*className={styles.button} onClick={() => window.open('https://drive.google.com/file/d/1A64LT5V8s2OE6-CKgCVp-NSxvBoVO65j/view?usp=sharing', '_target')}*/}
+                {/*>Download</button>*/}
+                {/*</div>*/}
                 {/*</section>*/}
 
                 <section>
                     <div className={styles.colLeft}>
-                        <img className={styles.shadow} src="/images/screenshot_1.jpg" />
+                        <img
+                            className={styles.shadow} src="/images/screenshot_1.jpg"
+                            onClick={() => window.open('https://drive.google.com/file/d/1ddWoEngf1Z7VaPK3q7uLo3OQkBv0SkbU/view', '_blank')}
+                        />
                     </div>
                     <div className={styles.colRight}>
                         <h2>Summary of Vision and Architecture</h2>
                         <button
-                            className={styles.button} onClick={() => window.open('https://drive.google.com/file/d/1ddWoEngf1Z7VaPK3q7uLo3OQkBv0SkbU/view', '_blank')}
-                        >Download</button>
+                            className={styles.button}
+                            onClick={() => window.open('https://drive.google.com/file/d/1ddWoEngf1Z7VaPK3q7uLo3OQkBv0SkbU/view', '_blank')}
+                        >Download
+                        </button>
                     </div>
                 </section>
 
                 {/*<section>*/}
-                    {/*<div className={styles.colLeft}>*/}
-                        {/*<img className={styles.shadow} src="/images/adoption-strategy.png" />*/}
-                    {/*</div>*/}
-                    {/*<div className={styles.colRight}>*/}
-                        {/*<h2><b>ADAPT</b> Adoption Strategy</h2>*/}
-                        {/*<button*/}
-                            {/*className={styles.button} onClick={() => window.open('https://drive.google.com/file/d/1F94zj_DlYuOmMOq8mQpT4SiWudEVzeAq/view?usp=sharing', '_blank')}*/}
-                        {/*>Download</button>*/}
-                    {/*</div>*/}
+                {/*<div className={styles.colLeft}>*/}
+                {/*<img className={styles.shadow} src="/images/adoption-strategy.png" />*/}
+                {/*</div>*/}
+                {/*<div className={styles.colRight}>*/}
+                {/*<h2><b>ADAPT</b> Adoption Strategy</h2>*/}
+                {/*<button*/}
+                {/*className={styles.button} onClick={() => window.open('https://drive.google.com/file/d/1F94zj_DlYuOmMOq8mQpT4SiWudEVzeAq/view?usp=sharing', '_blank')}*/}
+                {/*>Download</button>*/}
+                {/*</div>*/}
                 {/*</section>*/}
 
                 <section>
                     <div className={styles.colLeft}>
-                        <img className={styles.shadow} src="/images/mufl1p.png" />
+                        <img
+                            className={styles.shadow} src="/images/mufl1p.png"
+                            onClick={() => window.open('https://drive.google.com/file/d/1QjsU5qs5C9d8cZU90rtW8OvsZ60MiNm0/view?usp=sharing', '_blank')}
+                        />
                     </div>
                     <div className={styles.colRight}>
                         <h2><b>MUFL</b> Programming Primer</h2>
                         <button
-                            className={styles.button} onClick={() => window.open('https://drive.google.com/file/d/1QjsU5qs5C9d8cZU90rtW8OvsZ60MiNm0/view?usp=sharing', '_blank')}
-                        >Download</button>
+                            className={styles.button}
+                            onClick={() => window.open('https://drive.google.com/file/d/1QjsU5qs5C9d8cZU90rtW8OvsZ60MiNm0/view?usp=sharing', '_blank')}
+                        >Download
+                        </button>
                     </div>
                 </section>
 
                 {/*<section>*/}
-                    {/*<div className={styles.colLeft}>*/}
-                        {/*<img className={styles.shadow} src="/images/screenshot_3.jpg" />*/}
-                    {/*</div>*/}
-                    {/*<div className={styles.colRight}>*/}
-                        {/*<h2><b>MUFL</b> motivation and vision</h2>*/}
-                        {/*<button*/}
-                            {/*className={styles.button} onClick={() => window.open('https://drive.google.com/file/d/1o2ZHsyrEBDnGhuvzuitcTTBJLG67CMxR/view?usp=sharing', '_blank')}*/}
-                        {/*>Download</button>*/}
-                    {/*</div>*/}
+                {/*<div className={styles.colLeft}>*/}
+                {/*<img className={styles.shadow} src="/images/screenshot_3.jpg"/>*/}
+                {/*</div>*/}
+                {/*<div className={styles.colRight}>*/}
+                {/*<h2><b>MUFL</b> motivation and vision</h2>*/}
+                {/*<button*/}
+                {/*className={styles.button}*/}
+                {/*onClick={() => window.open('https://drive.google.com/file/d/1o2ZHsyrEBDnGhuvzuitcTTBJLG67CMxR/view?usp=sharing', '_blank')}*/}
+                {/*>Download*/}
+                {/*</button>*/}
+                {/*</div>*/}
                 {/*</section>*/}
             </div>
         )
