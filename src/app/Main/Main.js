a78e7d5dcd81d060813f30cfc9efd18e10f16c1f
denis.krasovskyi <denis.krasovskyi@gmail.com>
2018-12-17 12:37:14 +0200

    Adapt Story 16.12.2018

--- src/app/Main/Main.js
+++ src/app/Main/Main.js
@@ -2,31 +2,35 @@ import React, {Component} from 'react';
 import superagent from 'superagent';
 
 export default class Main extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    const styles = require('./Main.scss');
+    render() {
+        const styles = require('./Main.scss');
 
-    return (
-      <div className={styles.container}>
-        <img src={require("./img/home-bg.jpg")}></img>
-        <div className={styles.layout}>
-          <h1>
-            Build and launch your own decentralized network<br/>
-            <span>designed from the ground up to serve the needs of your users.</span>
-          </h1>
-          <h2>
-            <div>
-                ADAPT is a toolkit that will do to blockchain what compilers did to software and what TCP/IP did to connectivity.<br/>
+        return (
+            <div className={styles.container}>
+                <img src={require("./img/home-bg.jpg")}></img>
+                <div className={styles.layout}>
+                    <h1>
+                        Build and launch your own decentralized network<br/>
+                        <span>designed from the ground up to serve the needs of your users.</span>
+                    </h1>
+                    <h2>
+                        ADAPT is a software component toolkit for building a new generation of business-to-business and
+                        business-to-consumer blockchain/database systems. It offers unprecedented flexibility of
+                        decentralized software design and a suite of reusable components for rapid software development
+                        in this context. <a href='/contact'>Leave us your email for updates.</a>
+                        {/*<div>*/}
+                        {/*ADAPT is a toolkit that will do to blockchain what compilers did to software and what TCP/IP did to connectivity.<br/>*/}
+                        {/*</div>*/}
+                        {/*<div>*/}
+                        {/*It does not attempt to capture value at the base economic layer, offering its users the unprecedented flexibility of economic design. <a href='/contact'>Leave us your email for updates.</a>*/}
+                        {/*</div>*/}
+                    </h2>
+                </div>
             </div>
-            <div>
-                It does not attempt to capture value at the base economic layer, offering its users the unprecedented flexibility of economic design. <a href='/contact'>Leave us your email for updates.</a>
-            </div>
-          </h2>
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
--- src/app/WhitePaper/WhitePaper.js
+++ src/app/WhitePaper/WhitePaper.js
@@ -9,28 +9,30 @@ export default class WhitePaper extends Component {
         const styles = require('./WhitePaper.scss');
         return (
             <div className={styles.container}>
-                <section>
-                    <div className={styles.colLeft}>
-                        <img className={styles.shadow} src="/images/screenshot_2.jpg" />
-                    </div>
-                    <div className={styles.colRight}>
-                        <h2><b>ADAPT</b> white paper</h2>
-                        <button
-                            className={styles.button} onClick={() => window.open('https://drive.google.com/file/d/1A64LT5V8s2OE6-CKgCVp-NSxvBoVO65j/view?usp=sharing', '_target')}
-                        >Download</button>
-                    </div>
-                </section>
+                {/*<section>*/}
+                    {/*<div className={styles.colLeft}>*/}
+                        {/*<img className={styles.shadow} src="/images/screenshot_2.jpg" />*/}
+                    {/*</div>*/}
+                    {/*<div className={styles.colRight}>*/}
+                        {/*<h2><b>ADAPT</b> white paper</h2>*/}
+                        {/*<button*/}
+                            {/*className={styles.button} onClick={() => window.open('https://drive.google.com/file/d/1A64LT5V8s2OE6-CKgCVp-NSxvBoVO65j/view?usp=sharing', '_target')}*/}
+                        {/*>Download</button>*/}
+                    {/*</div>*/}
+                {/*</section>*/}
+
                 <section>
                     <div className={styles.colLeft}>
                         <img className={styles.shadow} src="/images/screenshot_1.jpg" />
                     </div>
                     <div className={styles.colRight}>
-                        <h2><b>ADAPT</b> Architecture Summary and Vision</h2>
+                        <h2>Summary of Vision and Architecture</h2>
                         <button
-                            className={styles.button} onClick={() => window.open('https://drive.google.com/open?id=1v3s8P9SFgywNsAOaOuemI5Tm3k9-7FN6', '_blank')}
+                            className={styles.button} onClick={() => window.open('https://drive.google.com/file/d/1ddWoEngf1Z7VaPK3q7uLo3OQkBv0SkbU/view', '_blank')}
                         >Download</button>
                     </div>
                 </section>
+
                 {/*<section>*/}
                     {/*<div className={styles.colLeft}>*/}
                         {/*<img className={styles.shadow} src="/images/adoption-strategy.png" />*/}
@@ -42,6 +44,7 @@ export default class WhitePaper extends Component {
                         {/*>Download</button>*/}
                     {/*</div>*/}
                 {/*</section>*/}
+
                 <section>
                     <div className={styles.colLeft}>
                         <img className={styles.shadow} src="/images/mufl1p.png" />
@@ -53,17 +56,18 @@ export default class WhitePaper extends Component {
                         >Download</button>
                     </div>
                 </section>
-                <section>
-                    <div className={styles.colLeft}>
-                        <img className={styles.shadow} src="/images/screenshot_3.jpg" />
-                    </div>
-                    <div className={styles.colRight}>
-                        <h2><b>MUFL</b> motivation and vision</h2>
-                        <button
-                            className={styles.button} onClick={() => window.open('https://drive.google.com/file/d/1o2ZHsyrEBDnGhuvzuitcTTBJLG67CMxR/view?usp=sharing', '_blank')}
-                        >Download</button>
-                    </div>
-                </section>
+
+                {/*<section>*/}
+                    {/*<div className={styles.colLeft}>*/}
+                        {/*<img className={styles.shadow} src="/images/screenshot_3.jpg" />*/}
+                    {/*</div>*/}
+                    {/*<div className={styles.colRight}>*/}
+                        {/*<h2><b>MUFL</b> motivation and vision</h2>*/}
+                        {/*<button*/}
+                            {/*className={styles.button} onClick={() => window.open('https://drive.google.com/file/d/1o2ZHsyrEBDnGhuvzuitcTTBJLG67CMxR/view?usp=sharing', '_blank')}*/}
+                        {/*>Download</button>*/}
+                    {/*</div>*/}
+                {/*</section>*/}
             </div>
         )
     }
